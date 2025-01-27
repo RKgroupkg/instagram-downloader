0ba992c898d4126a508bb3cc6d57a065776d0503
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-01-27 07:46:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const app = require('fastify')({
     logger: true
 });
-const keep_alive = require('./keep_alive.js')
 
+const keep_alive = require('./keep_alive.js'); // Import keep_alive.js
 const axios = require('axios');
 const telegramBot = require('node-telegram-bot-api');
 const bot = require('./controllers/insta');
 const insta = require('./controllers/bot');
 
+// Define the root route
+app.get('/', async (request, reply) => {
+    reply.send({ message: "Bot is running!" });
+});
 
-//run the server
+// Run the server
 const start = async () => {
     try {
         await app.listen({ port: 3000, host: '0.0.0.0' });
     } catch (err) {
-        fastify.log.error(err);
-        process.exit(1)
+        app.log.error(err);
+        process.exit(1);
     }
 }
 
